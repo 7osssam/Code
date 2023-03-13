e126258a044c05fe22d90a144b29eceb01327234
7osssam@protonmail.ch <7osssam@protonmail.ch>
2023-03-13 23:46:43 +0200

    update hi5hello30.c

--- C Essentials/hi5hello30.c
+++ C Essentials/hi5hello30.c
@@ -1,56 +1,5 @@
 #include <stdio.h>
 
-void str_print(char *str) //! same as printf("%s" , str);
-{
-  int i;
-  for (i = 0; str[i] != 0; i++)
-  {
-    printf("%c", str[i]);
-  }
-  printf("\n"); // new line
-}
-
-int strToInt(char *str)
-{
-  int i = 0, intger = 0;
-  for (i = 0; str[i] != 0; i++)
-  {
-    if (str[i] >= '0' && str[i] <= '9')
-      intger = intger * 10 + (str[i] - '0');
-  }
-  return intger;
-}
-
-void fun(void)
-{
-  static int y = 10;
-  y--;
-  printf("\n%d", y);
-}
-
-int *frameNum(char *str)
-{
-  int i, intger = 0, flag = 0, numcount = 0;
-  static int num[4];
-  for (i = 0; str[i] != 0; i++)
-  {
-    if (str[i] == ' ')
-    {
-      flag++; // flag
-      numcount++;
-    }
-    else
-    {
-      if (flag == numcount)
-      {
-        num[numcount] = num[numcount] * 10 + (str[i] - '0');
-      }
-
-    } //
-  }
-  return num;
-}
-
 void frame(char *str)
 {
   int i, flag = 0;
