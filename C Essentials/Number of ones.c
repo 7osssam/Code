0ff962cf56e75f690c4e9c2a60367d9b649f9fad
7osssam@protonmail.ch <7osssam@protonmail.ch>
2023-03-14 00:52:44 +0200

    update 'Number of ones.c'

--- C Essentials/Number of ones.c
+++ C Essentials/Number of ones.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #define INT_SIZE sizeof(int) * 8 /* Total number of bits in integer */
 
-int ones(int num )
+int ones(int num)
 {
-    int ones=0 , test ; // ,zeros=0
-    for (size_t i = 0; i < INT_SIZE; i++)
-    {
-          //test = num&(1<<i);
-        if (num & 1) // (num >> i ) & 1 
-            ones++;
+  int ones = 0, test; // ,zeros=0
+  for (size_t i = 0; i < INT_SIZE; i++)
+  {
+    // test = num&(1<<i);
+    if (num & 1) // (num >> i ) & 1
+      ones++;
 
-        num>>=1; // num = num >>1
-    }
-    return ones;
+    num >>= 1; // num = num >>1
+  }
+  return ones;
 }
-int main() {
-    unsigned int num = 2286;
+int main()
+{
+  unsigned int num = 2286;
 
-    printf("%d" , ones(num));
+  printf("%d", ones(num));
 
   return 0;
-    }
+}
