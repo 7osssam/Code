211779d67332d375127b635f6ab66008d6642312
7osssam@protonmail.ch <7osssam@protonmail.ch>
2023-03-13 23:48:00 +0200

    update 'convert string nums to integer and get sum.c'

--- C Essentials/convert string nums to integer and get sum.c
+++ C Essentials/convert string nums to integer and get sum.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
 
-void str_print(char *str) //! same as printf("%s" , str);
-{
-  int i;
-  for (i = 0; str[i] != 0; i++)
-  {
-    printf("%c", str[i]);
-  }
-  printf("\n"); // new line
-}
-
-int strToInt(char *str)
-{
-  int i = 0, intger = 0;
-  for (i = 0; str[i] != 0; i++)
-  {
-    if (str[i] >= '0' && str[i] <= '9')
-      intger = intger * 10 + (str[i] - '0');
-  }
-  return intger;
-}
-
-void fun(void)
-{
-  static int y = 10;
-  y--;
-  printf("\n%d", y);
-}
-
 int *frame(char *str)
 {
   int i, intger = 0, flag = 0, numcount = 0;
