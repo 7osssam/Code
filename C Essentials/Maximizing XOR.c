eb5f998e9a5553cac6507ea6d85b5f77850d7956
7osssam@protonmail.ch <7osssam@protonmail.ch>
2023-03-19 01:47:20 +0200

    update 'Maximizing XOR.c'

--- C Essentials/Maximizing XOR.c
+++ C Essentials/Maximizing XOR.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int  max_xor(int l , int r )
+int max_xor(int l, int r)
 {
     int xor = 0;
-    for ( int i = l ; i <= r ; i++ )
+    for (int i = l; i <= r; i++)
     {
         for (int j = i; j <= r; j++)
         {
-            if ( (i^j) > xor)
-             xor = i^j;
+            if ((i ^ j) > xor)
+                xor = i ^ j;
         }
-
     }
-return xor;
+    return xor;
 }
 
 int main()
 {
-    int l , r;
-    scanf("%d%d" ,&l ,&r);
-    printf("%d" , max_xor(l,r));
-  return 0;
-
+    int l, r;
+    scanf("%d%d", &l, &r);
+    printf("%d", max_xor(l, r));
+    return 0;
 }
