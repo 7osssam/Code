5b6a7625f78b6842a21f340438f14647afd9259d
Hossam <7osssam@protonmail.ch>
2023-04-01 02:10:48 +0200

    update Operators_Ex5.c

--- #MT_Exercises/Exercises_Part1/Operators_Ex5.c
+++ #MT_Exercises/Exercises_Part1/Operators_Ex5.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-    unsigned char var1 = 0x05, var2 = 0x0A;                          // 00000101 , 00001010 // 5 , 10
-    printf("Result of Andind the two variables: %x\n", var1 & var2); // 00000000 // 0x00 // hex
-    printf("Result of Oring the two variables: %x\n", var1 | var2);  // 00001111 // 0x0F // hex
-    printf("Result of Xor the two variables: %x\n", var1 ^ var2);    // 00001111 // 0x0F // hex
-    printf("Result of 1'complement for var1: %x\n", ~var1);          // 11111010 // 0xFA // hex
-    printf("Result of 1'complement for var2: %x\n", ~var2);          // 11110101 // 0xF5 // hex
+    unsigned char var1 = 0x05, var2 = 0x0A;                          // 0000 0101 , 0000 1010 // 5 , 10
+    printf("Result of Andind the two variables: %x\n", var1 & var2); // 0000 0000 // 0x00 // hex
+    printf("Result of Oring the two variables: %x\n", var1 | var2);  // 0000 1111 // 0x0F // hex
+    printf("Result of Xor the two variables: %x\n", var1 ^ var2);    // 0000 1111 // 0x0F // hex
+    printf("Result of 1'complement for var1: %x\n", ~var1);          // 1111 1010 // 0xFA // hex
+    printf("Result of 1'complement for var2: %x\n", ~var2);          // 1111 0101 // 0xF5 // hex
     return 0;
 }
