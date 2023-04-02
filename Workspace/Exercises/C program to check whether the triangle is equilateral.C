25ad04849cd949cfebbe7c718fa6b94e3418e706
Hossam <7osssam@protonmail.ch>
2023-04-03 01:36:59 +0200

    update 'C program to check whether the triangle is equilateral.C'

--- Workspace/Exercises/C program to check whether the triangle is equilateral.C
+++ Workspace/Exercises/C program to check whether the triangle is equilateral.C
@@ -5,6 +5,7 @@
 int main()
 {
     int side_1, side_2, side_3;
+    printf("Enter triangle sides ");
     scanf("%d%d%d", &side_1, &side_2, &side_3);
 
     if (side_1 == side_2 && side_1 == side_3)
