4f04a32306f6890db23b7255f215cb82eae4b6c4
Hossam <7osssam@protonmail.ch>
2023-04-06 02:52:39 +0200

    feat: create 1_5_ascii_value.c

---
+++ MT_Assignments/Assignment 1/1_5_ascii_value.c
@@ -0,0 +1,14 @@
+// (5) Write a program to print the ASCII value of a character
+// input by the user.
+
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter a char");
+    scanf("%c", &c);
+
+    printf("Binary ASCII Value%d", c); // %d is for decimal values
+    return 0;
+}
