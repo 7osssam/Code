335d17415d6d0f28810137f57808bc4e3f39e85f
Hossam <7osssam@protonmail.ch>
2023-04-07 06:56:17 +0200

    feat: create 3-sum-of-digits-of-a-five-digit-number.c

---
+++ Problem solving & Exercises/HackerRank/C/2 - Conditionals and Loops/3-sum-of-digits-of-a-five-digit-number.c
@@ -0,0 +1,29 @@
+//? hackerRank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    int mod, sum = 0;
+    scanf("%d", &n);
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     mod = n % 10;
+    //     n = n / 10;
+    //     sum += mod;
+    // }
+
+    while (n > 0)
+    {
+        mod = n % 10; // getting the last digit
+        n = n / 10;   // removing the last digit
+        sum += mod;   // adding the last digit to the sum
+    }
+    printf("%d", sum);
+
+    return 0;
+}
