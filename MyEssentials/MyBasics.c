57101298c9a57e4f9f1147131693546284ea81cc
Hossam <7osssam@protonmail.ch>
2023-04-07 23:11:52 +0200

    feat: create MyBasics.c

---
+++ MyEssentials/MyBasics.c
@@ -0,0 +1,14 @@
+void Swap_Advanced(int *num1, int *num2) // swap using pointers (pass by reference)
+{
+    *num1 = *num1 + *num2; // add x and y and store in x
+    *num2 = *num1 - *num2; // subtract y from x and store in y // x = x + y - y = x
+    *num1 = *num1 - *num2; // subtract y from x and store in x // y = x + y - x = y
+}
+
+void Swap(int *num1, int *num2) // swap using values (pass by value)
+{
+    int temp;
+    temp = *num1;  // save the value of num1
+    *num1 = *num2; // assign the value of num2 to num1
+    *num2 = temp;  // assign the saved value of num1 to num2
+}
