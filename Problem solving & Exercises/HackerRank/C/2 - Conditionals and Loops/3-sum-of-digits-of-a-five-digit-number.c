06c02781d606e1a7c3fe4b2bbb791d37b06561f7
Hossam <7osssam@protonmail.ch>
2023-04-07 07:05:35 +0200

    update 3-sum-of-digits-of-a-five-digit-number.c

--- Problem solving & Exercises/HackerRank/C/2 - Conditionals and Loops/3-sum-of-digits-of-a-five-digit-number.c
+++ Problem solving & Exercises/HackerRank/C/2 - Conditionals and Loops/3-sum-of-digits-of-a-five-digit-number.c
@@ -1,4 +1,4 @@
-//? hackerRank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
+//? https://hackerRank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
