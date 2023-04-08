56f4c0ed8a7bf99c33e261988c4e5536c9cf5ca5
Hossam <7osssam@protonmail.ch>
2023-04-08 09:28:40 +0200

    feat: create 4-a-very-big-sum.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/4-a-very-big-sum.c
@@ -0,0 +1,32 @@
+//? https://www.hackerrank.com/challenges/a-very-big-sum/problem
+#include <stdio.h>
+#include <stdlib.h>
+
+long aVeryBigSum(int size, long *arr)
+{
+    long sum = 0; // sum is a long int because the sum of the array elements can be very large
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    int size;
+    scanf("%d", &size);
+
+    long *arr = (long *)malloc(size * sizeof(long));
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%ld", &arr[i]);
+    }
+
+    long result = aVeryBigSum(size, arr);
+
+    printf("%ld\n", result);
+
+    return 0;
+}
