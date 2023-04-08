9f6031b5be59b1c70e9608e75a2f7381e9cca472
Hossam <7osssam@protonmail.ch>
2023-04-08 09:28:34 +0200

    feat: create 2-simple-array-sum.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/2-simple-array-sum.c
@@ -0,0 +1,24 @@
+//? https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size;
+    int sum = 0;
+    scanf("%d", &size);
+
+    int *ar = (int *)malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &ar[i]);
+        sum = sum + ar[i];
+    }
+
+    printf("%d", sum);
+
+    free(ar);
+    return 0;
+}
