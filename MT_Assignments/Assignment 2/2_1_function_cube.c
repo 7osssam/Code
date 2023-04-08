b944f2b693fb4557d80d9e16671932100f8c49ad
Hossam <7osssam@protonmail.ch>
2023-04-09 01:06:13 +0200

    feat: create 2_1_function_cube.c

---
+++ MT_Assignments/Assignment 2/2_1_function_cube.c
@@ -0,0 +1,19 @@
+// Write a C Function that prints the cube of any number
+#include <stdio.h>
+
+long cube(int num)
+{
+    return num * num * num; // return the cube of the number
+}
+
+int main()
+{
+    int num; // declare a variable to store the number
+
+    printf("Enter num : ");
+    scanf("%d", &num);
+
+    printf("Cude of %d = %ld", num, cube(num)); // call the function and print the result
+
+    return 0;
+}
