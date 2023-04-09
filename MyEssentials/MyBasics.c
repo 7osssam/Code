1fc0b3a235a57d04b875afefbc3ced36479fbe62
Hossam <7osssam@protonmail.ch>
2023-04-09 10:07:46 +0200

    update MyBasics.c

--- MyEssentials/MyBasics.c
+++ MyEssentials/MyBasics.c
@@ -46,3 +46,37 @@ int isPowerOfTwo_Advanced(int num) // time complexity: O(1)
         return 0; // number is NOT a power of 2
     }
 }
+//! ========================== fibonacci ==============================  //
+int fib(int num)
+{
+    // // Iterative approach
+    // // time complexity: O(n)
+    // int first = 0, second = 1, next;
+
+    // for (int i = 0; i < num; i++)
+    // {
+    //     if (i <= 1) // because fib of 0 = 0 and fib of 1 = 1
+    //     {
+    //         next = i;
+    //     }
+    //     else
+    //     {
+    //         next = first + second;
+    //         first = second;
+    //         second = next;
+    //     }
+    //     printf("%d ", next);
+    // }
+
+    // Recursive approach
+    // time complexity: O(2^n)
+    if (num == 0) // base case
+    {
+        return 0;
+    }
+    else if (num == 1) // base case
+    {
+        return 1;
+    }
+    return fib(num - 1) + fib(num - 2);
+}
