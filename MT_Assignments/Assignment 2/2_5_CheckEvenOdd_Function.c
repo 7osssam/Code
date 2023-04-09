7f973bb2e06b3c1bac74096f6fcfe06d6653c9f0
Hossam <7osssam@protonmail.ch>
2023-04-09 02:33:29 +0200

    update 2_5_CheckEvenOdd_Function.c

--- MT_Assignments/Assignment 2/2_5_CheckEvenOdd_Function.c
+++ MT_Assignments/Assignment 2/2_5_CheckEvenOdd_Function.c
@@ -1,4 +1,4 @@
-// Write a C function to check if the input is an even number or an odd number,
+// 5 - Write a C function to check if the input is an even number or an odd number,
 // if even number return 0 if odd number return 1.
 // Example:
 // Input 7, Output = 1 (Odd)
