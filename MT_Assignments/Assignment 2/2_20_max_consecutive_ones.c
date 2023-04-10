9b5b270307cdc77c0e8900a993198fa82008af3b
Hossam <7osssam@protonmail.ch>
2023-04-10 19:20:07 +0200

    feat: create 2_20_max_consecutive_ones.c

---
+++ MT_Assignments/Assignment 2/2_20_max_consecutive_ones.c
@@ -0,0 +1,40 @@
+// 20 - Write a C function to count the maximum number of consecutive 1’s in an unsigned 16 bits integer
+
+#include <stdio.h>
+#define SHORT_SIZE sizeof(unsigned short) * 8
+
+int maxOnes(int num)
+{
+    int max = 0;
+    int count = 0;
+    unsigned short mask = 0x8000; //
+    for (int i = 1; i <= SHORT_SIZE; i++)
+    {
+        if ((num & mask) != 0) //
+        {
+            count++;
+            if (count > max)
+            {
+                max = count;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
+        mask = mask >> 1;
+    }
+    return max;
+}
+
+int main()
+{
+    unsigned short num;
+
+    // printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("%d", maxOnes(num));
+
+    return 0;
+}
