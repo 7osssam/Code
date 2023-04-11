2ed0eb1f6ec54e7830de55e4b5f40b93d580a3aa
Hossam <7osssam@protonmail.ch>
2023-04-11 07:29:23 +0200

    feat: create 6-staircase-pattern.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/6-staircase-pattern.c
@@ -0,0 +1,29 @@
+//? https://www.hackerrank.com/challenges/staircase/problem
+#include <stdio.h>
+
+void pattern(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = i; j < num; j++) // not ( j <= num ) because The last line must have 0 spaces in it.
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    pattern(num);
+
+    return 0;
+}
