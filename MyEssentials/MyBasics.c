54627364b892a85b5a1e4237b2747541c4d44e3d
Hossam <7osssam@protonmail.ch>
2023-04-16 05:19:10 +0200

    update MyBasics.c : Bit Manipulation

--- MyEssentials/MyBasics.c
+++ MyEssentials/MyBasics.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define INT_SIZE sizeof(unsigned int) * 8
 
 //! ========================== Swap advanced ==========================  //
 void Swap_Advanced(int *num1, int *num2) // with no temp variable
@@ -104,4 +105,104 @@ int isPowerOfThree(int num) // time complexity: O(log n)
         num /= 3; // divide number by 3
     }
     return 1;
+}
+//! ========================== isPowerOfThree_Advanced ================== //
+int isPowerOfThree_Advanced(int num) // time complexity: O(1)
+{
+    if (num <= 0) // 0 is not a power of 3 and negative numbers are not power of 3
+        return 0;
+
+    if ((num % 3 == 0) && (num % 9 != 0)) // if number is a power of 3, then it will have only 1 bit set
+    {
+        return 1; // number is a power of 3
+    }
+    else
+    {
+        return 0; // number is NOT a power of 3
+    }
+}
+
+//! ==============================================================================//
+//!============================== Bit Manipulation ===============================//
+//! ==============================================================================//
+
+//! ========================== maxOnes ========================================== //
+// function to count the maximum number of consecutive 1's in an unsigned 32 bits integer
+int maxOnes(int num)
+{
+    int max = 0;
+    int count = 0;
+    unsigned int mask = 0x80000000;     // 32 bits mask
+    for (int i = 1; i <= INT_SIZE; i++) // loop through all bits
+    {
+        if (num & mask) // the result is a any value other than 0 (1 bit) //!any value --> true
+        {
+            count++;
+            if (count > max)
+            {
+                max = count;
+            }
+        }
+        else // if bit is exactly 0
+        {
+            count = 0; // reset count
+        }
+        mask = mask >> 1; // shift mask to the right
+    }
+    return max;
+}
+//! ========================== maxZeros ========================================== //
+// function to count the maximum number of consecutive 0's in an unsigned 32 bits integer
+int maxZeros(int num)
+{
+    // this function doesn't work if with numbers that take less than 32 bits because it will count the leading 0's
+    // so we only need to loop through all its bits
+    int max = 0;
+    int count = 0;
+    unsigned int mask = 0x80000000;     // 32 bits mask
+    for (int i = 1; i <= INT_SIZE; i++) // loop through all bits
+    {
+        if (num & mask) // the result is a any value other than 0 (1 bit) //!any value --> true
+        {
+            count = 0; // reset count
+        }
+        else if ((num & mask) == 0) // if bit is exactly 0
+        {
+            count++;
+            if (count > max)
+            {
+                max = count;
+            }
+        }
+        mask = mask >> 1; // shift mask to the right
+    }
+    return max;
+}
+
+//! ========================== maxZerosbetween ====================================== //
+// function to count the maximum number of consecutive 0's //!between 1’s in an unsigned 32 bits integer
+int maxZerosbetween(int num)
+{
+    int max = 0;
+    int count = 0;
+    unsigned int mask = 0x80000000; // 32 bits mask
+    for (int i = 1; i <= INT_SIZE; i++)
+    {
+        if (num & mask) // the result is a any value other than 0 (1 bit) //!any value --> true
+        {
+            //! we only set max once we have a 0 bit after a 1 bit
+            if (count > max)
+            {
+                max = count;
+            }
+            count = 0; // then we reset count
+        }
+        else if ((num & mask) == 0) // if bit is exactly 0
+        {
+            count++;
+        }
+
+        mask = mask >> 1;
+    }
+    return max;
 }
