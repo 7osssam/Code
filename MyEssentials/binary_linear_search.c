2a194be01359310ad6f7f2d58390f9aa46e01305
Hossam <7osssam@protonmail.ch>
2023-04-16 06:13:34 +0200

    update binary_linear_search.c

--- MyEssentials/binary_linear_search.c
+++ MyEssentials/binary_linear_search.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int Lsearch(int arr[], int x, int n) // linear search
+// time complexity: O(n)
+int LinearSearch(int arr[], int target, int arr_size)
 {
-   for (size_t i = 0; i < n; i++)
+   for (int i = 0; i < arr_size; i++)
    {
-      if (arr[i] == x) // if the element is found
+      if (arr[i] == target) // if the element is found
       {
          return i;
       }
@@ -12,22 +13,27 @@ int Lsearch(int arr[], int x, int n) // linear search
    return -1; // if the element is not found
 }
 
-int Bsearch(int arr[], int x, int first, int last) // binary search
+// time complexity: O(log n) //! only sorted array
+int BinarySearch(int arr[], int target, int arr_size)
 {
-   while (first <= last)
+   int first = 0;           // first index of the array
+   int last = arr_size - 1; // last index of the array
+
+   while (first <= last) // while the array is not empty
    {
       int mid = first + (last - first) / 2;
-      if (arr[mid] == x) // if the element is found
+
+      if (arr[mid] == target) // if the element is found
       {
          return mid;
       }
-      else if (arr[mid] > x) // right
+      else if (arr[mid] < target) // right side of the array
       {
-         last = mid - 1;
+         first = mid + 1; // mid + 1 because we already checked the mid element
       }
-      else if (arr[mid] < x) // left
+      else if (arr[mid] > target) // left side of the array
       {
-         first = mid + 1;
+         last = mid - 1; // mid - 1 because we already checked the mid element
       }
    }
    return -1; // if the element is not found
@@ -35,20 +41,21 @@ int Bsearch(int arr[], int x, int first, int last) // binary search
 
 int main()
 {
-   // int arr[] = {2, 23, 43, 1, 13, 11, 12, 22, 99, 100, 21};
-   int arr[] = {3, 4, 5, 7, 6, 8, 9};
-   int x;                                // the element to be searched
-   int n = sizeof(arr) / sizeof(arr[0]); // to get the size of the array
+   // int arr[] = {2, 23, 43, 1, 13, 11, 12, 22, 99, 100, 21}; // unsorted array (linear search)
+   int arr[] = {3, 4, 5, 7, 6, 8, 9};           // sorted array (binary search)
+   int target;                                  // the element to be searched
+   int arr_size = sizeof(arr) / sizeof(arr[0]); // to get the size of the array
+
    printf("Enter a num :");
-   scanf("%d", &x);
+   scanf("%d", &target);
 
-   // int res = Lsearch( arr , x , n ); // linear search
-   int res = Bsearch(arr, x, 0, n - 1); // binary search only sorted array
+   // int result = LinearSearch(arr, target, arr_size); // linear search
+   int result = BinarySearch(arr, target, arr_size); // binary search (only sorted array)
 
-   if (res == -1)
+   if (result == -1)
       printf("\nNOT FOUND");
    else
-      printf("\n%dst of the array", res + 1);
+      printf("\n%dst of the array", result + 1);
 
    return 0;
 }
