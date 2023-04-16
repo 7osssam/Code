94d1a1a75a7e906ea3012bbecccb5f260365cdcb
Hossam <7osssam@protonmail.ch>
2023-04-16 05:47:03 +0200

    chore: delete Notes.c, 'Number of ones.c' and Set_Clear_Toggle_Get.c

--- MyEssentials/Notes.c
+++
@@ -1,5 +0,0 @@
-// of the following are equivalent ways of accessing the array elements
-//*(arr + i); scanf("%d", arr + i);   | printf("%d", *(arr + i));
-//  *(i + arr); scanf("%d", i + arr);  | printf("%d", *(i + arr));
-//  arr[i]; scanf("%d", arr[i]);       | printf("%d", arr[i]);
-//  i[arr]; scanf("%d", i[arr]);       | printf("%d", i[arr]);
