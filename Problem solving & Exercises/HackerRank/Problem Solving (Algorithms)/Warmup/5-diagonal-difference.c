3bb6ca26526693f7858999001aa9022b7769e39b
Hossam <7osssam@protonmail.ch>
2023-04-17 10:35:02 +0200

    update 5-diagonal-difference.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/5-diagonal-difference.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/5-diagonal-difference.c
@@ -1,25 +1,26 @@
 // ? https://www.hackerrank.com/challenges/diagonal-difference/problem
+//
 #include <stdio.h>
 #include <stdlib.h>
 
 int diagonalDifference(int n, int **arr)
 {
     int sum = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // loop through the array
     {
         for (int j = 0; j < n; j++)
         {
-            if (i == j)
+            if (i == j) // if the index of the array is equal to the index of the array
             {
-                sum = sum + arr[i][j];
+                sum = sum + arr[i][j]; // add the element to the sum
             }
-            if (i == n - j - 1)
+            if (i == n - j - 1) // n - j - 1 is the index of the array from the end
             {
-                sum = sum - arr[i][j];
+                sum = sum - arr[i][j]; // subtract the element from the sum
             }
         }
     }
-    return abs(sum);
+    return abs(sum); // return the absolute value of the sum
 }
 
 int main()
@@ -28,6 +29,7 @@ int main()
     scanf("%d", &n);
 
     int **arr = (int **)malloc(n * sizeof(int *));
+
     for (int i = 0; i < n; i++)
     {
         *(arr + i) = (int *)malloc(n * sizeof(int));
