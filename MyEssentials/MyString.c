9d79dd1a64147e79525edc7047d5a50f8e9cd4f4
Hossam <7osssam@protonmail.ch>
2023-04-20 07:49:37 +0200

    update MyString.c

--- MyEssentials/MyString.c
+++ MyEssentials/MyString.c
@@ -296,6 +296,48 @@ void stringConcatenate(char *str1, char *str2, char *strCON) // concatenate two 
     strCON[strCounter] = 0; //! null at the end
 }
 
+char *superReducedString(char *s) // return the new string after removing the repeated characters
+{
+    int i, j, newPos; // newPos is the new position of the character after removing the repeated characters
+
+    for (i = 0; s[i] != '\0'; i++) //! <<
+    {
+        if (s[i] == s[i + 1]) // if the current character is equal to the next character
+        {
+            newPos = i; // set the new position to the current position
+            // newPos = 0 is wrong because it will start from the beginning of the string
+
+            for (j = i + 2; s[j] != '\0'; j++) // start from the next character after the repeated character
+            {
+                s[newPos] = s[j]; // copy the character to the new position
+                newPos++;         // increment the new position
+            }
+
+            s[newPos] = 0; //! null at the end
+
+            i = -1; //! reset the counter to start from the beginning
+            // i = 0 is wrong because i will be incremented to 1 //!^^ (i++)
+        }
+    }
+
+    if (s[0] == '\0') // if the string is empty (first character is null)
+    {
+        s = "Empty String"; // set the string to "Empty String"
+    }
+
+    return s;
+
+    // to return the new string, we create a char pointer (in the main function) and assign it to the return value
+    // fro example:
+    // char s[100];
+    // char *s_new;
+
+    // gets(s);
+    // s_new = superReducedString(s);
+
+    // puts(s_new);
+}
+
 int main()
 {
     unsigned char str[100] = " 2  ahlyhse 2 2  ahmed   ali ererwerewwemjk mohamedmo@@412fdff";
