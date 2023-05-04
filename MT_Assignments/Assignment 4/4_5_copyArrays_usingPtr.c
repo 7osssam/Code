ad6444751f38992a87c4d03f467f25063b4c5126
Hossam <7osssam@protonmail.ch>
2023-05-04 22:47:50 +0300

    update 4_5_copyArrays_usingPtr.c

--- MT_Assignments/Assignment 4/4_5_copyArrays_usingPtr.c
+++ MT_Assignments/Assignment 4/4_5_copyArrays_usingPtr.c
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 #define arr_size 5
+
+void copyArray(int *ptr1, int *ptr2, int size)
+{
+    for (int i = 0; i < arr_size; i++)
+    {
+        *ptr2 = *ptr1; // copy the value of the element
+
+        ptr1++; // point to the next element
+        ptr2++; // point to the next element
+    }
+}
+
 int main()
 {
     int arr1[arr_size] = {2, 4, 6, 62, 45};
@@ -11,13 +23,7 @@ int main()
     int *ptr1 = arr1;
     int *ptr2 = arr2;
 
-    for (int i = 0; i < arr_size; i++)
-    {
-        *ptr2 = *ptr1; // copy the value of the element
-
-        ptr1++; // point to the next element
-        ptr2++; // point to the next element
-    }
+    copyArray(ptr1, ptr2, arr_size);
 
     for (int i = 0; i < arr_size; i++)
     {
