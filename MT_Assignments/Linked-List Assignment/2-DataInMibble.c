41bc761f5f54d9cd8500bd9cff31c909108e5fcd
Hossam <7osssam@protonmail.ch>
2023-05-07 04:30:40 +0300

    feat: create 2-DataInMibble.c

---
+++ MT_Assignments/Linked-List Assignment/2-DataInMibble.c
@@ -0,0 +1,73 @@
+/*
+    (2) Write a C Function that returns the data of the middle node in a linked list
+    and in case the linked list contains only one node return the data inside this node.
+*/
+// ! solve with time complexity log(n)
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *nxPtr;
+
+} node;
+
+node *head = NULL;
+
+int DataInMibble(void)
+{
+    if (head == NULL)
+    {
+        // The linked list is empty
+        return -1; // or any other value to indicate an error
+    }
+    node *FastPtr = head;
+    node *SlowPtr = head;
+    while (FastPtr != NULL && FastPtr->nxPtr != NULL) // the order of the conditions matters in this case
+    {
+        FastPtr = FastPtr->nxPtr->nxPtr;
+        SlowPtr = SlowPtr->nxPtr;
+    }
+
+    return SlowPtr->data;
+}
+
+void DisplayList(void)
+{
+    node *ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf(" %d  ", ptr->data);
+        ptr = ptr->nxPtr;
+    }
+}
+
+void insertAtHead(int num)
+{
+
+    node *temp = (node *)malloc(sizeof(node));
+
+    temp->data = num;
+    temp->nxPtr = head;
+
+    head = temp;
+}
+
+int main()
+{
+    insertAtHead(60);
+    insertAtHead(5);
+    insertAtHead(23);
+
+    insertAtHead(50);
+    insertAtHead(60);
+
+    insertAtHead(10);
+    insertAtHead(3);
+    insertAtHead(11);
+    DisplayList();
+    printf("\n");
+    printf("Middle node is %d", DataInMibble());
+}
