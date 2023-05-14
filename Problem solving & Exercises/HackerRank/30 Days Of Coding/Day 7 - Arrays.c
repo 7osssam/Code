caf80b3d472e4a501c71555fdfe2d37ae8031273
Hossam <7osssam@protonmail.ch>
2023-05-14 20:53:14 +0300

    feat: create 'Day 7 - Arrays.c'

---
+++ Problem solving & Exercises/HackerRank/30 Days Of Coding/Day 7 - Arrays.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *num1, int *num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(&arr[i], &arr[n - i - 1]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
