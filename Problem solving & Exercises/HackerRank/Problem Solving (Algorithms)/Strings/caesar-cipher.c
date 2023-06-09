0ce637cb4b8faf2e55cd7a4b48b776ca10401a13
Hossam <7osssam@protonmail.ch>
2023-06-10 00:59:56 +0300

    feat: create caesar-cipher.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Strings/caesar-cipher.c
@@ -0,0 +1,62 @@
+//? https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *caesarCipher(char *str, int k) 
+{
+  int i;
+  
+  for (i = 0; str[i] != '\0'; i++) 
+  {
+    if ((str[i] >= 'a') && (str[i] <= 'z')) 
+    {
+        if(str[i] + k > 'z')
+        {
+           str[i] = str[i] - (26 - k); 
+
+        }
+        else 
+        {
+         str[i] = str[i] + k;
+
+        }
+    }
+    if ((str[i] >= 'A' && str[i] <= 'Z')) 
+    {
+        if(str[i] + k > 'Z')
+        {
+           str[i] = str[i] - (26 - k); 
+        }
+        else 
+        {
+          str[i] = str[i] + k;
+        }
+    }
+  }
+  return str;
+}
+
+int main() {
+  int n, k;
+  scanf("%d", &n);
+
+  char *str = (char *)malloc(n * sizeof(char));
+  char *encrypted = (char *)malloc(n * sizeof(char));
+  scanf("%s", str);
+
+  scanf("%d", &k);
+
+ 
+    while(k > 26)
+    {
+        k-=26;
+    }
+  
+  //printf("%d\n" , k); testing 
+  encrypted = caesarCipher(str, k);
+
+  puts(encrypted);
+  return 0;
+}
