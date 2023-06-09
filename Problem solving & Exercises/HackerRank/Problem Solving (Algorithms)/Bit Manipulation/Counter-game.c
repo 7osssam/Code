d2436c9a8a4fee00fa3a1bb3ca03b95144affbe7
Hossam <7osssam@protonmail.ch>
2023-06-10 01:00:13 +0300

    feat: create Counter-game.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Bit Manipulation/Counter-game.c
@@ -0,0 +1,80 @@
+//? https://www.hackerrank.com/challenges/counter-game/problem
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define isPowerOf2(num) (num && !(num & (num - 1)))
+
+//?? this function uses alot of floating point operations which is consuming alot of time and memory
+// unsigned long MSB(unsigned long num)
+// {
+//     static unsigned long mask = 1u << (sizeof(num) * 8u - 1u);
+//     while ((mask & num) == 0)
+//     {
+//         mask >>= 1;
+//     }
+//     return mask;
+// }
+
+unsigned long findLogBase2(unsigned long num)
+{
+    unsigned long r = 0;
+    while (num >>= 1UL)
+    {
+        r++;
+        // num >>= 1UL;
+    }
+    return r;
+}
+
+void Countergame(unsigned long num)
+{
+    unsigned long counter = -1; // even numbers are Louise // odd numbers are Richard
+    // unsigned long long k;
+    while (num != 1)
+    {
+        if (isPowerOf2(num)) // if num is power of 2
+        {
+            // num /= 2;
+            num >>= 1; // Divide by 2 using bitwise right-shift
+        }
+        else // if not
+        {
+            unsigned long msb = findLogBase2(num);
+            num -= (1U << msb);
+            // k = pow(2, floor(log2(num)));
+            // num -= k;
+        }
+        counter++;
+    }
+
+    if (counter % 2 == 0)
+    {
+        puts("Louise");
+    }
+    else
+    {
+        puts("Richard");
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    unsigned long *arr = (unsigned long *)malloc(sizeof(unsigned long) * n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lu", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        Countergame(arr[i]);
+    }
+
+    free(arr);
+}
