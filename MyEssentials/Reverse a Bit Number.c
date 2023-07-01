601767f493db15b82cf08b56529ae47ea365fea6
Hossam <7osssam@protonmail.ch>
2023-07-01 04:10:15 +0300

    update 'Reverse a Bit Number.c'

--- MyEssentials/Reverse a Bit Number.c
+++ MyEssentials/Reverse a Bit Number.c
@@ -4,20 +4,19 @@
 
 int main()
 {
-    unsigned char num = 212, num2 =0 ,count_2 = 7;
+    unsigned char num = 212, num2 = 0, count_2 = 7;
 
     for (size_t i = 0; i < 8; i++)
     {
-        printf("%d\n" , i);
-        if (num>>i & 1 )
+        printf("%d\n", i);
+        if (num >> i & 1)
         {
-            num2 = num2 | 1 << count_2 ;
+            num2 = num2 | 1 << count_2;
         }
-        printf("%d\n" , count_2);
+        printf("%d\n", count_2);
         count_2 = count_2 - 1;
     }
-    printf("%d" , num2);
+    printf("%d", num2);
 
-
-  return 0;
+    return 0;
 }
