cebc84b4902a8ff36e9428f32074de5c13749b52
Hossam <7osssam@protonmail.ch>
2023-07-01 04:08:58 +0300

    feat: create CamelCase.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Strings/CamelCase.c
@@ -0,0 +1,28 @@
+//? https://www.hackerrank.com/challenges/camelcase/problem/
+// count the number of words starting with a capital letter in a string (CamelCase)
+#include <stdio.h>
+
+int CamelCase(char const *str) // Function to count the number of words starting with a capital letter in a string
+{
+	int i;
+	int wordCounter = 1; // The first word is always counted
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z') // If the character is a capital letter
+		{
+			wordCounter++;
+		}
+	}
+	return wordCounter;
+}
+
+int main()
+{
+	char str[100000]; // The string is limited to 100000 characters
+
+	fgets(str, 100000, stdin);
+
+	printf("%d", CamelCase(str));
+
+	return 0;
+}
