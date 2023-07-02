b64e12bf269b5aa75c7915000397eb342b637981
Hossam <7osssam@protonmail.ch>
2023-07-02 23:05:44 +0300

    update 4-a-very-big-sum.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/4-a-very-big-sum.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/4-a-very-big-sum.c
@@ -1,34 +1,43 @@
-//? https://www.hackerrank.com/challenges/a-very-big-sum/problem
-// usage of long int to store the sum of the array elements
+/*
+ ? Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+ * Difficulty: Easy
+ * Name: A Very Big Sum
+ *
+ * Tags: Algorithms, Warmup
+ *
+ * Summary:
+ * sum of array elements
+ * + usage of long int to store the sum of the array elements
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 long aVeryBigSum(int size, long *arr)
 {
-    long sum = 0; // sum is a long int because the sum of the array elements can be very large
-    for (int i = 0; i < size; i++)
-    {
-        sum = sum + arr[i];
-    }
-    return sum;
+	long sum = 0; // sum is a long int because the sum of the array elements can be very large
+	for (int i = 0; i < size; i++)
+	{
+		sum = sum + arr[i];
+	}
+	return sum;
 }
 
 int main()
 {
-    int size;
-    scanf("%d", &size);
+	int size;
+	scanf("%d", &size);
 
-    long *arr = (long *)malloc(size * sizeof(long));
+	long *arr = (long *)malloc(size * sizeof(long));
 
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%ld", &arr[i]);
-    }
+	for (int i = 0; i < size; i++)
+	{
+		scanf("%ld", &arr[i]);
+	}
 
-    long result = aVeryBigSum(size, arr);
+	long result = aVeryBigSum(size, arr);
 
-    printf("%ld\n", result);
+	printf("%ld\n", result);
 
-    return 0;
+	return 0;
 }
