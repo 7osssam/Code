d5c3fa2cdace53f5fdf1e217d2c9d87a9baf05e7
Hossam <7osssam@protonmail.ch>
2023-07-02 22:53:26 +0300

    update breaking-best-and-worst-records.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Implementation/breaking-best-and-worst-records.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Implementation/breaking-best-and-worst-records.c
@@ -1,50 +1,58 @@
-//? https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+/*
+ ? Link: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+ * Difficulty: Easy
+ * Name: Breaking the Records
+ *
+ * Tags: Algorithms, Implementation
+ *
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 //////////////////////!
 int *breakingRecords(int scores_count, const int *scores)
 {
-    int i;
-    static int result[2] = {0};
-    int max = scores[0];
-    int min = scores[0];
-    for (i = 0; i < scores_count; i++)
-    {
-        if (max > scores[i])
-        {
-            result[1]++;
-            max = scores[i];
-        }
-        if (min < scores[i])
-        {
-            result[0]++;
-            min = scores[i];
-        }
-    }
-    return result;
+	int i;
+	static int result[2] = {0};
+	int max = scores[0];
+	int min = scores[0];
+	for (i = 0; i < scores_count; i++)
+	{
+		if (max > scores[i])
+		{
+			result[1]++;
+			max = scores[i];
+		}
+		if (min < scores[i])
+		{
+			result[0]++;
+			min = scores[i];
+		}
+	}
+	return result;
 }
 //////////////////////!
 int main()
 {
-    int n;
+	int n;
 
-    scanf("%d", &n);
-    int *scores = (int *)malloc(n * sizeof(int));
+	scanf("%d", &n);
+	int *scores = (int *)malloc(n * sizeof(int));
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &scores[i]);
-    }
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &scores[i]);
+	}
 
-    int *result = breakingRecords(n, scores);
+	int *result = breakingRecords(n, scores);
 
-    for (int i = 0; i < 2; i++)
-    {
-        printf("%d ", *(result + i));
-    }
+	for (int i = 0; i < 2; i++)
+	{
+		printf("%d ", *(result + i));
+	}
 
-    free(scores);
+	free(scores);
 
-    return 0;
+	return 0;
 }
