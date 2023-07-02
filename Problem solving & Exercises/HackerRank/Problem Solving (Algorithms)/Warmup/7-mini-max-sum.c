64015541fa815865839da03ba4aea1d490c6f19f
Hossam <7osssam@protonmail.ch>
2023-07-02 23:05:52 +0300

    update 7-mini-max-sum.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/7-mini-max-sum.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/7-mini-max-sum.c
@@ -1,7 +1,17 @@
-//? https://www.hackerrank.com/challenges/mini-max-sum/problem
-// find the minimum and maximum sums of 4 out of 5 numbers in an array
-// Todo: the minimum sum is the sum of all elements - maximum number
-// Todo: maximum sum is the sum of all elements - minimum number
+/*
+ ? Link: https://www.hackerrank.com/challenges/mini-max-sum/problem
+ * Difficulty: Easy
+ * Name: Mini-Max Sum
+ *
+ * Tags: Algorithms, Warmup
+ *
+ * Summary:
+ * find the minimum and maximum sums of 4 out of 5 numbers in an array
+ *
+ ! Note:
+ * the minimum sum is the sum of all elements - maximum number
+ * maximum sum is the sum of all elements - minimum number
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,33 +19,33 @@
 
 int main()
 {
-    // dynamic array allocation for
-    int *arr = (int *)malloc(arr_size * sizeof(int));
+	// dynamic array allocation for
+	int *arr = (int *)malloc(arr_size * sizeof(int));
 
-    // scan array elements
-    for (int i = 0; i < arr_size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+	// scan array elements
+	for (int i = 0; i < arr_size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
 
-    // minimum sum = sum of all elements - maximum number
-    // maximum sum = sum of all elements - minimum number
-    long sum = 0;                    // sum of all elements //!long to avoid overflow
-    int mini = arr[0], max = arr[0]; // minimum and maximum numbers in the array
+	// minimum sum = sum of all elements - maximum number
+	// maximum sum = sum of all elements - minimum number
+	long sum = 0;					 // sum of all elements //!long to avoid overflow
+	int mini = arr[0], max = arr[0]; // minimum and maximum numbers in the array
 
-    for (int i = 0; i < arr_size; i++)
-    {
-        sum = sum + arr[i];
-        if (arr[i] < mini) // if the current element is smaller than the minimum
-        {
-            mini = arr[i]; // update the minimum
-        }
-        if (arr[i] > max) // if the current element is bigger than the maximum
-        {
-            max = arr[i]; // update the maximum
-        }
-    }
-    printf("%ld %ld", (sum - max), (sum - mini)); // print the minimum and maximum sums
+	for (int i = 0; i < arr_size; i++)
+	{
+		sum = sum + arr[i];
+		if (arr[i] < mini) // if the current element is smaller than the minimum
+		{
+			mini = arr[i]; // update the minimum
+		}
+		if (arr[i] > max) // if the current element is bigger than the maximum
+		{
+			max = arr[i]; // update the maximum
+		}
+	}
+	printf("%ld %ld", (sum - max), (sum - mini)); // print the minimum and maximum sums
 
-    free(arr); // free the allocated memory
+	free(arr); // free the allocated memory
 }
