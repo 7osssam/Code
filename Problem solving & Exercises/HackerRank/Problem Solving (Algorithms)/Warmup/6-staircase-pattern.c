e4deb084915652833a8bb07218067c8b4766282e
Hossam <7osssam@protonmail.ch>
2023-07-02 23:05:49 +0300

    update 6-staircase-pattern.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/6-staircase-pattern.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/6-staircase-pattern.c
@@ -1,37 +1,46 @@
-//? https://www.hackerrank.com/challenges/staircase/problem
-// print a staircase pattern
-//       #
-//      ##
-//     ###
-//    ####
-//   #####
-//  ######
-// #######
+/*
+ ? Link: https://www.hackerrank.com/challenges/staircase/problem
+ * Difficulty: Easy
+ * Name: Staircase
+ *
+ * Tags: Algorithms, Warmup
+ *
+ * Summary:
+ * print a staircase pattern
+ *  	   #
+ *  	  ##
+ *  	 ###
+ *  	####
+ *     #####
+ *    ######
+ *   #######
+ */
+
 #include <stdio.h>
 
 void pattern(int num)
 {
-    for (int i = 1; i <= num; i++)
-    {
-        for (int j = i; j < num; j++) // not ( j <= num ) because The last line must have 0 spaces in it.
-        {
-            printf(" ");
-        }
-        for (int j = 1; j <= i; j++)
-        {
-            printf("#");
-        }
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = i; j < num; j++) // not ( j <= num ) because The last line must have 0 spaces in it.
+		{
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++)
+		{
+			printf("#");
+		}
 
-        printf("\n");
-    }
+		printf("\n");
+	}
 }
 
 int main()
 {
-    int num;
-    scanf("%d", &num);
+	int num;
+	scanf("%d", &num);
 
-    pattern(num);
+	pattern(num);
 
-    return 0;
+	return 0;
 }
