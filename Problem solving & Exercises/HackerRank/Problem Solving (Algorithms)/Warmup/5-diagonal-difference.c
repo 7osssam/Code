544cdb05b2f812b273eea99267f047016557ceb8
Hossam <7osssam@protonmail.ch>
2023-07-02 23:05:46 +0300

    update 5-diagonal-difference.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/5-diagonal-difference.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/5-diagonal-difference.c
@@ -1,55 +1,63 @@
-// ? https://www.hackerrank.com/challenges/diagonal-difference/problem
-//
+/*
+ ? Link: https://www.hackerrank.com/challenges/diagonal-difference/problem
+ * Difficulty: Easy
+ * Name: Diagonal Difference
+ *
+ * Tags: Algorithms, Warmup
+ *
+ * Summary:
+ * sum of 2D array elements, diagonal difference
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 int diagonalDifference(int n, int **arr)
 {
-    int sum = 0;
-    for (int i = 0; i < n; i++) // loop through the array
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j) // if the index of the array is equal to the index of the array
-            {
-                sum = sum + arr[i][j]; // add the element to the sum
-            }
-            if (i == n - j - 1) // n - j - 1 is the index of the array from the end
-            {
-                sum = sum - arr[i][j]; // subtract the element from the sum
-            }
-        }
-    }
-    return abs(sum); // return the absolute value of the sum
+	int sum = 0;
+	for (int i = 0; i < n; i++) // loop through the array
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j) // if the index of the array is equal to the index of the array
+			{
+				sum = sum + arr[i][j]; // add the element to the sum
+			}
+			if (i == n - j - 1) // n - j - 1 is the index of the array from the end
+			{
+				sum = sum - arr[i][j]; // subtract the element from the sum
+			}
+		}
+	}
+	return abs(sum); // return the absolute value of the sum
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+	int n;
+	scanf("%d", &n);
 
-    int **arr = (int **)malloc(n * sizeof(int *));
+	int **arr = (int **)malloc(n * sizeof(int *));
 
-    for (int i = 0; i < n; i++)
-    {
-        *(arr + i) = (int *)malloc(n * sizeof(int));
-        for (int j = 0; j < n; j++)
-        {
-            //  scanf("%d", (*(arr + i) + j));
-            scanf("%d", &arr[i][j]);
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = (int *)malloc(n * sizeof(int));
+		for (int j = 0; j < n; j++)
+		{
+			//  scanf("%d", (*(arr + i) + j));
+			scanf("%d", &arr[i][j]);
+		}
+	}
 
-    int result = diagonalDifference(n, arr);
+	int result = diagonalDifference(n, arr);
 
-    printf("%d\n", result);
+	printf("%d\n", result);
 
-    for (int i = 0; i < n; i++)
-    {
-        // free(*(arr + i));
-        free(arr[i]);
-    }
-    free(arr);
+	for (int i = 0; i < n; i++)
+	{
+		// free(*(arr + i));
+		free(arr[i]);
+	}
+	free(arr);
 
-    return 0;
+	return 0;
 }
