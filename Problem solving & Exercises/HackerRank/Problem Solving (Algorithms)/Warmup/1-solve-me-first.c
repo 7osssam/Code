29c3f8f1fdd1637d63ef58e0a08fa46d3716349a
Hossam <7osssam@protonmail.ch>
2023-07-02 23:05:35 +0300

    update 1-solve-me-first.c

--- Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/1-solve-me-first.c
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Warmup/1-solve-me-first.c
@@ -1,20 +1,28 @@
-//? https://www.hackerrank.com/challenges/solve-me-first/problem
-// sum of two integers
-#include <stdio.h>
-#include <string.h>
+/*
+ ? Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+ * Difficulty: Easy
+ * Name: Solve Me First
+ *
+ * Tags: Algorithms, Warmup
+ *
+ * Summary:
+ * sum of two integers
+ */
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int solveMeFirst(int a, int b)
 {
-    return a + b; // :)
+	return a + b; // :)
 }
 int main()
 {
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
-    int sum;
-    sum = solveMeFirst(num1, num2);
-    printf("%d", sum);
-    return 0;
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	int sum;
+	sum = solveMeFirst(num1, num2);
+	printf("%d", sum);
+	return 0;
 }
