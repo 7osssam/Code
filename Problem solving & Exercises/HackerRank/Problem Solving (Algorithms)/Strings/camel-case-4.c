cbc0615e1dac6617b5c86ec711e2336bd7cea380
Hossam <7osssam@protonmail.ch>
2023-07-02 06:59:04 +0300

    feat: create camel-case-4.c

---
+++ Problem solving & Exercises/HackerRank/Problem Solving (Algorithms)/Strings/camel-case-4.c
@@ -0,0 +1,110 @@
+//? https://www.hackerrank.com/challenges/three-month-preparation-kit-camel-case/problem
+
+#include <stdio.h>
+#include <string.h>
+
+// Split mode: LargeSoftwareBook --> large software book
+char *splitMode(char *str)
+{
+	static char CleanStr[100];
+	int position = 0;
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+			if (i != 0)
+			{
+				CleanStr[position] = ' ';
+				position++;
+			}
+		}
+		if (str[i] == '(')
+		{
+			break;
+		}
+		CleanStr[position] = str[i];
+		position++;
+	}
+	CleanStr[position] = '\0';
+
+	return CleanStr;
+}
+
+// Combine mode: plasticCup() --> plastic cup
+char *combineMode(char *str, char type)
+{
+	static char CleanStr[100];
+	int position = 0;
+	int i = 0;
+
+	if (type == 'C')
+	{
+		str[0] = str[0] - 32;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			i++;
+			str[i] = str[i] - 32;
+		}
+
+		CleanStr[position] = str[i];
+		position++;
+	}
+
+	if (type == 'M')
+	{
+		position = position - 1; // Adjust position by 1
+
+		CleanStr[position++] = '('; // Add '(' before space
+		CleanStr[position++] = ')'; // Add ')' after space
+	}
+
+	CleanStr[position] = '\0';
+
+	return CleanStr;
+}
+
+int main()
+{
+	char str[100];
+	char CleanStr[100];
+
+	while (gets(str) != NULL) // fgets is not working
+	{
+
+		char operation = str[0]; // S or C
+		char type = str[2];		 // M or C or V
+
+		// copying in cleanStr
+		int i = 0;
+		int position = 4;
+		while (str[position] != '\0')
+		{
+			CleanStr[i] = str[position];
+			i++;
+			position++;
+		}
+		CleanStr[i] = '\0'; // null at the end
+
+		char *returnChar = NULL;
+
+		if (operation == 'S') // split
+		{
+			returnChar = splitMode(CleanStr);
+		}
+		else if (operation == 'C') // combine
+		{
+			returnChar = combineMode(CleanStr, type);
+		}
+
+		puts(returnChar); // print the string
+	}
+
+	return 0;
+}
