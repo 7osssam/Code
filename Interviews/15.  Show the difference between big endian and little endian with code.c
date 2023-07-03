9196c19bb0606aecb24e1b9f22e671b1734fcfae
Hossam <7osssam@protonmail.ch>
2023-07-03 03:43:32 +0300

    delete 103 files

--- Interviews/15.  Show the difference between big endian and little endian with code.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  int x = 0x12345678;
-  char *ptr =(char*) &x;
-  printf("%x",*ptr);
-    return 0;
-}
--- Interviews/31.  How to count the size of the array if it is unknown..c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-   int arr[] = {2,4,6,3,8,10};
-  int s = sizeof(arr)/sizeof(arr[0]);
-  printf("The size of array = %d",s);
-    return 0;
-}
