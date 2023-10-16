58d0e7ec7f600616e64ef8b4da98e6f6328716d6
Hossam <7osssam@protonmail.ch>
2023-10-17 00:57:08 +0300

    >>

--- Projects/Vehicle Control System/Dashboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef DASHBOARD_H
-#define DASHBOARD_H
-
-#include "SensorsMenu.h"
-
-void TurnON(VEHICLE_t *data);
-void TurnOFF(VEHICLE_t *data);
-
-#endif /* DASHBOARD_H */
--- Projects/Vehicle Control System/README.md
+++
@@ -1,3 +0,0 @@
-`gcc Mini_Project.c SensorsMenu.c Dashboard.c -o program`
-
-`.\program.exe`
