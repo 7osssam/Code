88ca8e01c0801866b2fbf4d20c25d761765ae0f0
Hossam <7osssam@protonmail.ch>
2023-10-18 19:13:25 +0300

    update Dashboard.c and Dashboard.h

--- Projects/Vehicle Control System/Dashboard.c
+++ Projects/Vehicle Control System/Dashboard.c
@@ -1,72 +1,156 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "Dashboard.h"
 
-void TurnON(VEHICLE_t *data)
+/* Global variable to store the vehicle data with initial values */
+VEHICLE_t DATA = {.Engine_state = OFF,
+				  .AC_state = OFF,
+				  .Engine_Temp_state = OFF,
+				  .Speed = 0,
+				  .Room_Temp = 25,
+				  .Engine_Temp_data = 90};
+
+void TurnON(VEHICLE_t* data)
 {
-    char option; // option entered by the user
+	int option;
+	int Turn_on_flag = false;
 
-    while (1) // infinite loop to keep the program running
-    {
-        printf("\033[33m==================================\n"); // yellow color for the menu
-
-        printf("a. Turn off the engine. \n");
-        printf("b. Set the traffic light color. \n");
-        printf("c. Set the room temperature. \n");
-#if WITH_ENGINE_TEMP_CONTROLLER // check if the engine temperature controller is enabled
-        printf("d. Set the engine temperature. \n");
+	while (Turn_on_flag == false)
+	{
+		TerminalColor_YELLOW();
+		printf("==================================\n");
+		printf("|| 1. Turn off the engine. \n");
+		printf("|| 2. Set the traffic light color. \n");
+		printf("|| 3. Set the room temperature. \n");
+#if WITH_ENGINE_TEMP_CONTROLLER
+		printf("|| 4. Set the engine temperature. \n");
 #endif
+		printf("==================================\n");
+		TerminalColor_RESET();
 
-        printf("==================================\033[0m\n"); // reset color to default
+#if WITH_ENGINE_TEMP_CONTROLLER
+		printf("Enter an option (1-4): ");
+#else
+		printf("Enter an option (1-3): ");
+#endif
+		int scanfResult = scanf("%d", &option);
 
-        fflush(stdout); // flush the output buffer to print the message immediately
-        scanf(" %c", &option);
+		if (scanfResult == true)
+		{
+#if WITH_ENGINE_TEMP_CONTROLLER
+			if (1 <= option && option <= 4)
+#else
+			if (1 <= option && option <= 3)
+#endif
+			{
+				switch (option)
+				{
+					case 1:
+						TurnOFF(data);
+						Turn_on_flag = true;
+						break;
 
-        switch (option)
-        {
-        case 'a':
-            TurnOFF(data);
-            return; // return to the main menu after turning off the engine
+					case 2:
+						trafficLight(data);
+						break;
 
-        case 'b':
-            trafficLight(data);
-            break;
-
-        case 'c':
-            RoomTemperature(data);
-            break;
+					case 3:
+						RoomTemperature(data);
+						break;
 
 #if WITH_ENGINE_TEMP_CONTROLLER // check if the engine temperature controller is enabled
-        case 'd':
-            EngineTemperature(data);
-            break;
+					case 4:
+						EngineTemperature(data);
+						break;
 #endif
+				}
+			}
+			else
+			{
+				WrongOption_Message();
+			}
+		}
+		else
+		{
+			// Clear the input buffer
+			while (getchar() != '\n')
+				;
+			WrongOption_Message();
+		}
 
-        default:
-            printf("\033[41m"); // red color for the error message
-            printf(" Wrong Option! try again... ");
-            printf("\033[0m\n"); // reset color to default
-            break;
-        }
-
-        MenuDisplay(data);
-    }
+		MenuDisplay(data);
+	}
 }
 
-void TurnOFF(VEHICLE_t *data)
+void TurnOFF(VEHICLE_t* data)
 {
-    /***********************
-    no need to set the DATA values to 0 because the menu only appears when the engine is ON but this
-    will keep the code more readable and the simulation more realistic
-    ************************/
+	/* NOTE:
+     * no need to set the DATA values to 0 because the menu only appears when the engine is ON but this
+     * will keep the code more readable and the simulation more realistic
+     */
 
-    data->Engine_state = OFF;
-    data->Speed = 0;
-    data->AC_state = OFF;
-    data->Room_Temp = 0;
-    data->Engine_Temp_state = OFF;
-    data->Engine_Temp_data = 0;
+	data->Engine_state = OFF;
+	data->Speed = 0;
+	data->AC_state = OFF;
+	data->Room_Temp = 0;
+	data->Engine_Temp_state = OFF;
+	data->Engine_Temp_data = 0;
 
-    printf("\033[31m"); // red color for the message
-    printf("\nVehicle Engine is off\n\n");
-    printf("\033[0m\n"); // reset color to default
+	TerminalColor_RED();
+	printf("\n==== Vehicle Engine is off ====\n");
+	TerminalColor_RESET();
+}
+
+void DashboardMenu(void)
+{
+	int option;
+
+	TerminalColor_BLUE();
+
+	printf("====================================\n||\n||");
+	TerminalColor_YELLOW();
+	printf("       Vehicle Control System    \n");
+	TerminalColor_BLUE();
+	printf("||\n|| 1. Turn on the vehicle engine\n");
+	printf("|| 2. Turn off the vehicle engine\n");
+	printf("|| 3. Quit the system\n");
+	printf("====================================\n");
+
+	TerminalColor_RESET();
+
+	printf("Enter an option (1-3): ");
+	int scanfResult = scanf("%d", &option);
+
+	if (scanfResult == true) // if the user entered a number not a character
+	{
+		if (1 <= option && option <= 3)
+		{
+			switch (option)
+			{
+				case 1:
+					system("cls"); // for Windows terminal
+					TurnON(&DATA);
+					break;
+
+				case 2:
+					TurnOFF(&DATA);
+					break;
+
+				case 3:
+					exit(0); // exit the program
+					break;
+			}
+		}
+		else
+		{
+			WrongOption_Message();
+		}
+	}
+	else
+	{
+		// Clear the input buffer
+		while (getchar() != '\n')
+			;
+		WrongOption_Message();
+	}
 }
--- Projects/Vehicle Control System/Dashboard.h
+++ Projects/Vehicle Control System/Dashboard.h
@@ -3,7 +3,30 @@
 
 #include "SensorsMenu.h"
 
-void TurnON(VEHICLE_t *data);
-void TurnOFF(VEHICLE_t *data);
+/**
+ * The function "TurnON" allows the user to select various options such as turning off the engine,
+ * setting the traffic light color, setting the room temperature, and setting the engine temperature
+ * (if enabled), and displays a menu for the user to choose from.
+ * 
+ * @param data The parameter "data" is of type VEHICLE_t*, which is a pointer to a structure or object
+ * of type VEHICLE_t. This structure or object likely contains information about a vehicle, such as its
+ * engine status, traffic light color, room temperature, and engine temperature. The TurnON function is
+ */
+void TurnON(VEHICLE_t* data);
+
+/**
+ * The function TurnOFF sets the values of various data members of a VEHICLE_t struct to indicate that
+ * the vehicle's engine is turned off.
+ * 
+ * @param data A pointer to a structure of type VEHICLE_t.
+ */
+void TurnOFF(VEHICLE_t* data);
+
+/**
+ * The function `DashboardMenu` displays a menu for a vehicle control system and allows the user to
+ * select options such as turning on/off the vehicle engine or quitting the system.
+ * @note This function is called from the main function in the file `Mini_Project.c`.
+ */
+void DashboardMenu(void);
 
 #endif /* DASHBOARD_H */
