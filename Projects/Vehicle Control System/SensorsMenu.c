4580b0649473212feb58cbaeede6d13f99c30883
Hossam <7osssam@protonmail.ch>
2023-10-18 19:13:37 +0300

    update SensorsMenu.c and SensorsMenu.h

--- Projects/Vehicle Control System/SensorsMenu.c
+++ Projects/Vehicle Control System/SensorsMenu.c
@@ -1,100 +1,133 @@
 #include "SensorsMenu.h"
 
-void trafficLight(VEHICLE_t *vehicle_data)
+#include <stdio.h>
+
+/******************************************************************
+ * 					Terminal Color and Message					  *
+ ******************************************************************/
+void TerminalColor_RED(void)
 {
-  char input;
-
-  while (1)
-  { // infinite loop to keep the program running to handle invalid inputs
-    printf("Enter traffic Light (G: Green, O: Orange, R: Red):\n");
-
-    fflush(stdout); // flush the output buffer to print the message immediately
-    scanf(" %c", &input);
-
-    switch (input)
-    {
-    case 'G':
-    case 'g':
-      vehicle_data->Speed = 100;
-      break;
-
-    case 'O':
-    case 'o':
-      vehicle_data->Speed = 30;
-
-      // special case for the AC and Engine Temp sensors
-
-      vehicle_data->AC_state = ON;
-      vehicle_data->Room_Temp = vehicle_data->Room_Temp * (5 / 4) + 1;
-
-      vehicle_data->Engine_Temp_state = ON;
-      vehicle_data->Engine_Temp_data = vehicle_data->Engine_Temp_data * (5 / 4) + 1;
-      break;
-
-    case 'R':
-    case 'r':
-      vehicle_data->Speed = 0;
-      break;
-
-    default:
-      printf("\033[41m"); // red color for the error message
-      printf(" Wrong Option! try again ");
-      printf("\033[0m\n"); // reset color to default
-
-      continue; // skip to next iteration of the while loop
-    }
-
-    break; // break out of the while loop if a valid option is entered
-  }
+	printf("\033[41m");
+}
+void TerminalColor_YELLOW(void)
+{
+	printf("\033[33m");
+}
+void TerminalColor_BLUE(void)
+{
+	printf("\033[34m");
+}
+void TerminalColor_GREEN(void)
+{
+	printf("\033[32m");
+}
+void TerminalColor_RESET(void)
+{
+	printf("\033[0m\n"); // reset color to default
+}
+void WrongOption_Message(void)
+{
+	TerminalColor_RED();
+	printf("\n Wrong Option! try again");
+	TerminalColor_RESET();
 }
 
-void RoomTemperature(VEHICLE_t *vehicle_data)
+/******************************************************************
+ * 						Sensors Menu Functions					  *
+ ******************************************************************/
+
+void trafficLight(VEHICLE_t* vehicle_data)
 {
-  int input;
+	char input;
 
-  printf("Enter Room Temperature:\n");
+	while (1)
+	{ // infinite loop to keep the program running to handle invalid inputs
+		printf("Enter traffic Light (G: Green, O: Orange, R: Red):\n");
+		scanf(" %c", &input);
 
-  fflush(stdout); // flush the output buffer to print the message immediately
-  scanf(" %d", &input);
+		switch (input)
+		{
+			case 'G':
+			case 'g':
+				vehicle_data->Speed = 100;
+				break;
 
-  if (input < 10 || input > 30)
-  {
-    vehicle_data->AC_state = ON;
-    vehicle_data->Room_Temp = 20;
-  }
+			case 'O':
+			case 'o':
+				vehicle_data->Speed = 30;
+
+				// special case for the AC and Engine Temp sensors
+
+				vehicle_data->AC_state = ON;
+				vehicle_data->Room_Temp = vehicle_data->Room_Temp * (5 / 4) + 1;
+
+				vehicle_data->Engine_Temp_state = ON;
+				vehicle_data->Engine_Temp_data = vehicle_data->Engine_Temp_data * (5 / 4) + 1;
+				break;
+
+			case 'R':
+			case 'r':
+				vehicle_data->Speed = 0;
+				break;
+
+			default:
+				WrongOption_Message();
+				continue; // skip to next iteration of the while loop
+		}
+
+		break; // break out of the while loop if a valid option is entered
+	}
+}
+
+void RoomTemperature(VEHICLE_t* vehicle_data)
+{
+	int input;
+
+	printf("Enter Room Temperature:\n");
+	scanf(" %d", &input);
+
+	if (input < 10 || input > 30)
+	{
+		vehicle_data->AC_state = ON;
+		vehicle_data->Room_Temp = 20;
+	}
 }
 
 #if WITH_ENGINE_TEMP_CONTROLLER // check if the engine temperature controller is enabled
-void EngineTemperature(VEHICLE_t *vehicle_data)
+void EngineTemperature(VEHICLE_t* vehicle_data)
 {
-  int input;
+	int input;
 
-  printf("Enter Engine Temperature:\n");
+	printf("Enter Engine Temperature:\n");
 
-  fflush(stdout); // flush the output buffer to print the message immediately
-  scanf(" %d", &input);
+	scanf(" %d", &input);
 
-  if (input < 100 || input > 150)
-  {
-    vehicle_data->Engine_Temp_state = ON;
-    vehicle_data->Engine_Temp_data = 20;
-  }
+	if (input < 100 || input > 150)
+	{
+		vehicle_data->Engine_Temp_state = ON;
+		vehicle_data->Engine_Temp_data = 20;
+	}
 }
 #endif // end of WITH_ENGINE_TEMP_CONTROLLER check
 
-void MenuDisplay(const VEHICLE_t *vehicle_data)
+void MenuDisplay(const VEHICLE_t* vehicle_data)
 {
-  printf("\033[32m__________________________________\n\n"); // green color for the menu
-  printf("Engine state: %s\n", vehicle_data->Engine_state ? "ON" : "OFF");
-  printf("Vehicle Speed: %d Km/hr\n", vehicle_data->Speed);
+	TerminalColor_GREEN();
+	printf("____________________________________\n|\n|");
+	TerminalColor_YELLOW();
+	printf("       Vehicle Dashboard    \n");
+	TerminalColor_GREEN();
+	printf("|\n|   Engine state: %s\n", vehicle_data->Engine_state ? "ON" : "OFF");
+	printf("|   Vehicle Speed: %d Km/hr\n", vehicle_data->Speed);
 
-  printf("AC state: %s\n", vehicle_data->AC_state ? "ON" : "OFF");
-  printf("Room Temperature: %d C\n", vehicle_data->Room_Temp);
+	printf("|   AC state: %s\n", vehicle_data->AC_state ? "ON" : "OFF");
+	printf("|   Room Temperature: %d C", vehicle_data->Room_Temp);
 
 #if WITH_ENGINE_TEMP_CONTROLLER // this code is only compiled if WITH_ENGINE_TEMP_CONTROLLER is set to 1
-  printf("Engine Temperature state: %s\n", vehicle_data->Engine_Temp_state ? "ON" : "OFF");
-  printf("Engine Temperature: %d C\n", vehicle_data->Engine_Temp_data);
+	printf("\n|   Engine Temperature state: %s\n", vehicle_data->Engine_Temp_state ? "ON" : "OFF");
+	printf("|   Engine Temperature: %d C", vehicle_data->Engine_Temp_data);
 #endif // end of WITH_ENGINE_TEMP_CONTROLLER check
 
-  printf("__________________________________\033[0m\n"); // reset color to default
+	printf("\n|____________________________________\n"); // reset color to default
+	TerminalColor_RESET();
 }
--- Projects/Vehicle Control System/SensorsMenu.h
+++ Projects/Vehicle Control System/SensorsMenu.h
@@ -1,33 +1,83 @@
 #ifndef SENSORS_MENU_H_
 #define SENSORS_MENU_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
-#define WITH_ENGINE_TEMP_CONTROLLER 0 // set this to 0 to disable the engine temperature controller
+/******************************************************************
+ * 						Preprocessor Macros					  	  *
+ ******************************************************************/
+#define ON							true
+#define OFF							false
 
-/// ================================ user defined data types ================================ ///
-typedef enum
-{
-	OFF,
-	ON
-} Bool;
+/******************************************************************
+ * 						System Configuration					  *
+ ******************************************************************/
+#define WITH_ENGINE_TEMP_CONTROLLER (OFF)
 
 typedef struct
 {
-	Bool Engine_state;
-	Bool AC_state;
-	Bool Engine_Temp_state;
-	int Speed;
-	int Room_Temp;
-	int Engine_Temp_data;
+	bool Engine_state;
+	bool AC_state;
+	bool Engine_Temp_state;
+	int	 Speed;
+	int	 Room_Temp;
+	int	 Engine_Temp_data;
 } VEHICLE_t;
 
-void trafficLight(VEHICLE_t *vehicle_data);
-void RoomTemperature(VEHICLE_t *vehicle_data);
+/******************************************************************
+ * 					Terminal Color and Message					  *
+ ******************************************************************/
+void TerminalColor_RED(void);
+void TerminalColor_YELLOW(void);
+void TerminalColor_BLUE(void);
+void TerminalColor_GREEN(void);
+void TerminalColor_RESET(void);
+void WrongOption_Message(void);
+
+/******************************************************************
+ * 						Sensors Menu Functions					  *
+ ******************************************************************/
+
+/**
+ * The trafficLight function takes user input for the current traffic light color and updates the speed
+ * and other data of a vehicle accordingly.
+ * 
+ * @param vehicle_data A pointer to a structure of type VEHICLE_t, which contains information about the
+ * vehicle such as speed, AC state, room temperature, and engine temperature.
+ */
+void trafficLight(VEHICLE_t* vehicle_data);
+
+/**
+ * The function sets the AC state and room temperature of a vehicle based on user input.
+ * 
+ * @param vehicle_data A pointer to a structure of type VEHICLE_t, which likely contains information
+ * about a vehicle, such as its current state and temperature.
+ */
+void RoomTemperature(VEHICLE_t* vehicle_data);
+
 #if WITH_ENGINE_TEMP_CONTROLLER
-void EngineTemperature(VEHICLE_t *vehicle_data);
+
+/**
+ * The function EngineTemperature takes user input for engine temperature and updates the vehicle_data
+ * structure accordingly.
+ * 
+ * @param vehicle_data A pointer to a structure of type VEHICLE_t, which likely contains information
+ * about a vehicle, such as its engine temperature.
+ * 
+ * @note This function is only compiled if the preprocessor macro WITH_ENGINE_TEMP_CONTROLLER is set to 1.
+ */
+void EngineTemperature(VEHICLE_t* vehicle_data);
 #endif
-void MenuDisplay(const VEHICLE_t *vehicle_data);
+
+/**
+ * The function MenuDisplay displays the vehicle dashboard information including engine state, vehicle
+ * speed, AC state, room temperature, and engine temperature (if engine temperature controller is
+ * enabled).
+ * 
+ * @param vehicle_data The parameter `vehicle_data` is a pointer to a structure of type `VEHICLE_t`.
+ * This structure contains information about the vehicle, such as the engine state, vehicle speed, AC
+ * state, room temperature, engine temperature state, and engine temperature data.
+ */
+void MenuDisplay(const VEHICLE_t* vehicle_data);
 
 #endif // SENSORS_MENU_H_
