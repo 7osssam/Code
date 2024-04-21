4ba0075ba74b6d7def396f2af3f181fc3ca8cc75
Hossam <7osssam@protonmail.ch>
2024-04-21 11:58:30 +0200

    Add test cases for BankAccountSystem

--- ITIDA-IMT/CPP/Lec (4)/4- Bank Account System/main.cpp
+++ ITIDA-IMT/CPP/Lec (4)/4- Bank Account System/main.cpp
@@ -1,6 +1,7 @@
 #include "Bank.hpp"
 #include <iostream>
 #include <string>
+#include <cassert>
 
 enum Option
 {
@@ -60,7 +61,7 @@ void HandleDepositFunds(Bank& bank_account)
 
 	// Find the account by account number
 	bool found = false;
-	for (auto& acc: const_cast<std::vector<BankAccount>&>(bank_account.getAccounts()))
+	for (BankAccount& acc: bank_account.getAccounts())
 	{
 		if (acc.getAccountNumber() == accountNumber)
 		{
@@ -89,7 +90,7 @@ void HandleWithdrawFunds(Bank& bank_account)
 
 	// Find the account by account number
 	bool found = false;
-	for (auto& acc: const_cast<std::vector<BankAccount>&>(bank_account.getAccounts()))
+	for (auto& acc: bank_account.getAccounts())
 	{
 		if (acc.getAccountNumber() == accountNumber)
 		{
@@ -111,8 +112,58 @@ void HandleWithdrawFunds(Bank& bank_account)
 	}
 }
 
+void test_BankAccountSystem()
+{
+	Bank bank_account = Bank{};
+
+	double acc1_amount = 1000.0;
+	double acc2_amount = 2000.0;
+
+	double TotalBalance = 0.0;
+
+	double deposit_test = 0.0;
+	double withdraw_test = 0.0;
+
+	// Test creating an account
+	BankAccount account1{123, "Hossam", "SILVER", acc1_amount};
+
+	assert(account1.getAccountBalance() == acc1_amount);
+
+	// Test depositing funds
+	deposit_test = 500.0;
+	account1.deposit(deposit_test);
+	acc1_amount += deposit_test;
+	assert(account1.getAccountBalance() == acc1_amount);
+
+	// Test withdrawing funds
+	withdraw_test = 300.0;
+	account1.withdraw(withdraw_test);
+	acc1_amount -= withdraw_test;
+	assert(account1.getAccountBalance() == acc1_amount);
+
+	// Test adding an account to the bank and getting total balance
+	bank_account.addAccount(account1);
+	TotalBalance = acc1_amount;
+	assert(bank_account.getTotalBalance() == TotalBalance);
+
+	// Test creating another account
+	BankAccount account2{456, "Kareem", "GOLD", acc2_amount};
+	bank_account.addAccount(account2);
+	TotalBalance += acc2_amount;
+	assert(bank_account.getTotalBalance() == TotalBalance);
+
+	// Test sorting accounts --> extra test
+	bank_account.SortAccounts();
+	const auto& accounts = bank_account.getAccounts();
+	assert(accounts[0].getAccountNumber() == 123);
+	assert(accounts[1].getAccountNumber() == 456);
+
+	std::cout << "===== All tests passed =====\n";
+}
+
 int main()
 {
+	test_BankAccountSystem();
 	Bank bank_account{};
 	int	 choice;
 	//Option choice; --> need opterator overloading for enum class
